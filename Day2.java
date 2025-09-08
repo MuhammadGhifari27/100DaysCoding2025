2b32cbfd0c1dc5b1ee06a9178b4a41fa3b96a93c
Jihyo <muhammadgio938@gmail.com>
2025-09-09 07:11:37 +0800

    Day2.java

---
+++ Day2.java
@@ -0,0 +1,11 @@
+public class days2 {
+    public static void main(String[] args) {
+        System.out.print("ini tampilan print");
+        System.out.println("");//untuk garis baru
+        System.out.println("ini untuk println");
+        
+        String saya = "Gyo";
+        int umur = 19;
+        System.out.printf("saya %s dan saya berumur %d tahun.", saya, umur);
+    }
+}

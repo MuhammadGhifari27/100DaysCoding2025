fffa81d1701bedfd2a8b324514fafe9e5f49579c
Jihyo <muhammadgio938@gmail.com>
2025-09-11 00:59:12 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,11 @@
+public class Day4 {
+    public static void main(String[] args) {
+      //\n
+        System.out.println("Nama\nAlamat");
+      //\t
+        System.out.println("Nama:\tFajar");
+        System.out.println("Umur:\t25");
+      //\"
+        System.out.println("Dia bilang, \"Halo, dunia!\"");
+    }
+          }

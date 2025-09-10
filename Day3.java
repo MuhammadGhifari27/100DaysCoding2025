a124a3b8be965e5041e31169d8a5a10eac976ccf
Jihyo <muhammadgio938@gmail.com>
2025-09-10 11:46:14 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,10 @@
+public class Day3 {
+    public static void main(String[] args) {
+        System.out.println("Komentar 1");
+        //System.out.println("Komentar Baris Tunggal");
+        System.out.println("Komentar 2");
+        /*Ini adalah contoh komentar multiline Bisa dipakai
+        untuk menjelaskan kode dalam beberapa baris.
+        */
+    }
+}

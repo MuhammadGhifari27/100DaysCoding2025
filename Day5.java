460d88e35df02f5e5f8bbb1675870bea9001865a
Jihyo <muhammadgio938@gmail.com>
2025-09-11 19:36:12 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,26 @@
+public class Day5 {
+    public static void main(String[] args) {
+        //1.byte
+        //Ukuran: 8-bit
+        //Rentang Nilai:-128 hingga 127
+        byte umur = 30;
+        //2.short
+        //Ukuran: 16-bit
+        //Rentang Nilai:-32,768 hingga 32,767
+        short gajiperjam = 3000;
+        //3.int
+        //Ukuran:32-bit
+        //Rentang Nilai:-2.1 miliar hingga 2.1 miliar
+        int HargaMobil = 350000000;
+        //4.long
+        //Ukuran:
+        //Rentang Nilai:
+        long populasiDunia = 8100000000L;
+        
+        System.out.println("Umur :"+ umur +"(Hemat memori untuk angka Kecil)");
+        System.out.println("Gaji Perjam :"+ gajiperjam +"(Untuk angka ribuan)");
+        System.out.println("Harga mobil :"+ HargaMobil +"(Paling umum untuk Jutaan/miliar)");
+        System.out.println("PopulasiManusia :"+ populasiDunia +"L(Untuk angka yang sangat besar)");
+    }
+ 
+}

a5776fef297c00e0e6737111260d0e2ede610325
Jihyo <muhammadgio938@gmail.com>
2025-09-12 20:05:24 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,9 @@
+public class TipeDataPecahan {
+
+    public static void main(String[] args) {
+        float nilaiFloat = 3.14f;
+        double nilaiDouble = 99.12345;
+        System.out.println("Nilai float: " + nilaiFloat);
+        System.out.println("Nilai double: " + nilaiDouble);
+    }
+}

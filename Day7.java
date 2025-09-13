3f0b3038563570426fd238362223caecb902c87a
Jihyo <muhammadgio938@gmail.com>
2025-09-13 20:48:11 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,10 @@
+public class Day7 {
+    public static void main(String[] args) {
+        char Gyo = 'G';
+        boolean benar = true;
+        boolean salah = false;
+        System.out.println("Angka Pertama :"+Gyo);
+        System.out.println("Hasilnya :"+benar);
+        System.out.println("Hasilnya :"+salah);
+    }
+          }

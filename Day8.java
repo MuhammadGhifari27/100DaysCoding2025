33e5cc2f4ecc81a0f3f3b3ddd9c5b643f3f065e0
Jihyo <muhammadgio938@gmail.com>
2025-09-14 23:58:55 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,11 @@
+public class Day8 {
+    public static void main(String[] args) {
+        //variabel yang akan di gabukan
+        String Namadepan = "Park";
+        String Namabelang = "Jihyoung";
+        //Penggabungan Veriabel
+        String Namalengkap =Namadepan+" "+Namabelang;
+        //Memanggil variabel yang di gabung
+        System.out.println("Perkenalkan Nama Saya :"+ Namalengkap);
+    }
+}

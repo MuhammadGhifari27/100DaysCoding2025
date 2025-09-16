b62061145044fda1677d183fb242c4dae5b70caf
Jihyo <muhammadgio938@gmail.com>
2025-09-16 12:39:20 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,11 @@
+public class Day9 {
+   
+    public static void main(String[] args) {
+       int skor = 55;
+        System.out.println("Sebelum update:");
+        System.out.println("Nilai skor : " + skor);
+        skor = 95;
+        System.out.println("Setelah update:");
+        System.out.println("Nilai skor: " + skor);
+    }
+}

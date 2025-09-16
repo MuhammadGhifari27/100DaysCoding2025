3e3967ac2ca247462efa49b0fbc03082c368665b
Jihyo <muhammadgio938@gmail.com>
2025-09-17 00:50:44 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,13 @@
+public class Day10 {
+    public static void main(String[] args) {
+        //nilai konstantaa
+        final int X = 9;
+        final double PI = 3.14;
+
+        System.out.println("Nilai X : " + X);
+        System.out.println("Nilai PI: " + PI);
+        
+        //tidak bisa update atau ubah nilai yang sudah ada variabel finalnhya
+        // X = 10; 
+    }
+}

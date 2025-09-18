b3303df2cf4981050a8803a0dd3d1dac06e4e81e
Jihyo <muhammadgio938@gmail.com>
2025-09-18 20:00:12 +0800

    Day11.java

---
+++ Day11.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Day11 {
+    public static void main(String[] args) {
+        Scanner gyo = new Scanner (System.in);
+        System.out.print("masukan nama :");
+        String nama = gyo.nextLine();
+        System.out.println("hola "+nama);
+    }
+}

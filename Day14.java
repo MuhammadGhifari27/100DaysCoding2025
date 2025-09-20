575ac51f6bfcb31a01c2109704efa06fea5688ae
Jihyo <muhammadgio938@gmail.com>
2025-09-20 18:33:09 +0800

    Day14.java

---
+++ Day14.java
@@ -0,0 +1,7 @@
+public class Day14 {
+    public static void main(String[] args) {
+        String umur = "19";
+        int umursaya = Integer.parseInt(umur);
+        System.out.println("umur saya :"+umursaya);
+    }
+}

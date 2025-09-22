427d289654cf3fda73fb2ecc41a80b1320998665
Jihyo <muhammadgio938@gmail.com>
2025-09-22 22:52:04 +0800

    Day17.java

---
+++ Day17.java
@@ -0,0 +1,8 @@
+public class Day17 {
+    public static void main(String[] args) {
+        int g = 9;
+        int j = 2;
+        int sisabagi = g % j;
+        System.out.println("hasil sisabagi :\n"+sisabagi);
+    }
+}

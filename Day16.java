1d266da145aefc4578be10d556fcd52e282a1b3d
Jihyo <muhammadgio938@gmail.com>
2025-09-22 22:48:32 +0800

    Day16.java

---
+++ Day16.java
@@ -0,0 +1,10 @@
+public class Day16 {
+    public static void main(String[] args) {
+        int g = 10 ;
+        int h = 2;
+        int perkalian = g * h;
+        int pembagian = g / h;
+        System.out.println("hasil perkalian :"+perkalian);
+        System.out.println("hasil pembagian :"+pembagian);
+    }
+}

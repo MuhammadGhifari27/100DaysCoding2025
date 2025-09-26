be8fcf9e619793d193fe416a02fd34e14ab7659c
Jihyo <muhammadgio938@gmail.com>
2025-09-26 09:50:09 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,9 @@
+package Day;
+public class Day19 {
+public static void main(String[] args) {
+int suhuInt = 280;
+byte suhuByte = (byte) suhuInt;
+System.out.println("Suhu int: " + suhuInt);
+System.out.println("Suhu byte setelah konversi: " + suhuByte);
+}
+}

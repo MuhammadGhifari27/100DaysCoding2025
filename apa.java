1040ccc7f19b0fe48770fff946fe251831d21a85
Jihyo <muhammadgio938@gmail.com>
2025-09-26 17:31:38 +0800

    apa.java

--- apa.java
+++ apa.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class soal {
     public static void main(String[] args) {
         Scanner gyo = new Scanner (System.in);
-        System.out.println("masukan nilai :");
+        System.out.print("masukan nilai :");
             int g = gyo.nextInt();
             if(g%2==0){
-                System.out.println(g+"genap");
+                System.out.println("angka "+g+" adalah genap");
             }else{
-                System.out.println(g+"ganjil");
+                System.out.println("angka "+g+" adalah ganjil");
             }
         }
     }

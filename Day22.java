d7e5c7fe6c3fdd1d2e03f3c2da27bd88a419cc0d
Jihyo <muhammadgio938@gmail.com>
2025-09-29 17:48:06 +0800

    Day22.java

---
+++ Day22.java
@@ -0,0 +1,8 @@
+package day1;
+public class Day22 {
+    public static void main(String[] args) {
+        int g = 4;
+        int luas = g * g;
+        System.out.println("luas :"+luas);
+    }
+}

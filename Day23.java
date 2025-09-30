85e2123fd591fceec58191c1dd8298565706bfec
Jihyo <muhammadgio938@gmail.com>
2025-09-30 14:25:30 +0800

    Day23.java

---
+++ Day23.java
@@ -0,0 +1,8 @@
+public class Day23 {
+    public static void main(String[] args) {
+        int lebar = 8;
+        int panjang = 10;
+        int luas = lebar * panjang;
+        System.out.println("luas persegi panjang :"+luas);
+    }
+}

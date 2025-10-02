4a08277c02bab14d099d247ee809f21c835ee73a
Jihyo <muhammadgio938@gmail.com>
2025-10-02 19:20:03 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,23 @@
+public class Day25 {
+    public static void main(String[] args) {
+       int a = 10;
+       int b = a++;
+        System.out.println("nilai a :"+a);
+        System.out.println("nilai b :"+b);
+        int x = 10;
+        int y = ++x;
+        System.out.println("nilai x :"+x);
+        System.out.println("nilai y :"+y);
+        int g = 10;
+        int w = g--;
+        System.out.println("nilai g :"+g);
+        System.out.println("nilai w :"+w);
+        int d = 10;
+        int s = --d;
+        System.out.println("nilai d :"+d);
+        System.out.println("nilai s :"+s);
+        
+       
+        
+    }
+}

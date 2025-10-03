0b3225af159f9009e481ca8b71ee02a971241125
Jihyo <muhammadgio938@gmail.com>
2025-10-03 20:24:12 +0800

    Day26.java

---
+++ Day26.java
@@ -0,0 +1,22 @@
+public class Day26 {
+    public static void main(String[] args) {
+        int g;
+        g = 2;
+        int q = 3;
+        q += 2;
+        int w = 2;
+        w -= 1;
+        int e = 2;
+        e *= 2;
+        int v = 4;
+        v /= 2;
+        int z = 3;
+        z %= 2;
+        System.out.println(g);
+        System.out.println(q);
+        System.out.println(w);
+        System.out.println(e);
+        System.out.println(v);
+        System.out.println(z);
+    }
+}

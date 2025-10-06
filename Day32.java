cf87b18681cbf5d5e48dbd2cdb0a9e7409bdf96d
Jihyo <muhammadgio938@gmail.com>
2025-10-06 17:21:16 +0800

    Day32.java

---
+++ Day32.java
@@ -0,0 +1,10 @@
+public class Day32 {
+    public static void main(String[] args) {
+        int umur = 19;
+        int rumu = 30;
+        System.out.println(umur < 18 || rumu < 29);
+        System.out.println(umur > 20 || rumu > 29);
+        System.out.println(umur > 18 || rumu > 31);
+        
+    }
+}

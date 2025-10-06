1cb18501171c0a49269e5b05e8ce49414501135c
Jihyo <muhammadgio938@gmail.com>
2025-10-06 16:43:51 +0800

    Day29.java

---
+++ Day29.java
@@ -0,0 +1,8 @@
+public class Day29 {
+    public static void main(String[] args) {
+        int g = 1;
+        int h = 2;
+        System.out.println(g < h);
+        System.out.println(g > h);
+    }
+}

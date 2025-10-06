952faa3845b92eb86bb9f39a096896a93783369d
Jihyo <muhammadgio938@gmail.com>
2025-10-06 17:06:24 +0800

    Day30.java

---
+++ Day30.java
@@ -0,0 +1,8 @@
+public class Day30 {
+    public static void main(String[] args) {
+        int g = 4;
+        int k = 1;
+        System.out.println(g <= k);
+        System.out.println(g >= k);
+    }
+}

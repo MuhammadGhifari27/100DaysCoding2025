84175b6dc955e29af3b4a3e9d8a0d4bd80911fd2
Jihyo <muhammadgio938@gmail.com>
2025-10-10 18:24:00 +0800

    Day33.java

---
+++ Day33.java
@@ -0,0 +1,9 @@
+public class Day33 {
+    public static void main(String[] args) {
+        boolean hujan = true;
+        System.out.println(!hujan);  // hasilnya: false
+
+        boolean cerah = false;
+        System.out.println(!cerah);  // hasilnya: true
+    }
+}

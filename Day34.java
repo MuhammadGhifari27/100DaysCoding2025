d74764d7bbaa0cd7155dac40859ea5afd5998e78
Jihyo <muhammadgio938@gmail.com>
2025-10-11 04:04:04 +0800

    Day34.java

---
+++ Day34.java
@@ -0,0 +1,8 @@
+public class KombinasiOperatorSederhana {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 4;
+        int c = 2;
+        System.out.println((a + b) > (b * c) && (a - c) != 0);
+    }
+}

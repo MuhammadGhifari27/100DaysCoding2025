1f6dfb0556274a83872a74d213942f3a52d709d8
Jihyo <muhammadgio938@gmail.com>
2025-10-12 14:47:38 +0800

    Day35.java

---
+++ Day35.java
@@ -0,0 +1,12 @@
+public class Day35 {
+    public static void main(String[] args) {
+        int umur = 20;
+
+        
+        if (umur >= 17) {
+            System.out.println("Anda sudah dewasa dan boleh memiliki KTP.");
+        }
+        
+        System.out.println("Pengecekan umur selesai.");
+    }
+}

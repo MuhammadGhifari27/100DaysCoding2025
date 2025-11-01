782aa996c5bc9b635e41485a11ed7eeea1c7ff3d
Jihyo <muhammadgio938@gmail.com>
2025-11-01 18:21:12 +0800

    Day55.java

---
+++ Day55.java
@@ -0,0 +1,47 @@
+package day1;
+
+import java.util.Scanner;
+
+public class Day55 {
+    public static void main(String[] args) {
+        Scanner g = new Scanner (System.in);
+        
+          //soal 1
+//        System.out.println("batas bewah :");
+//        int k = g.nextInt();
+//        System.out.println("batas atas :");
+//        int q = g.nextInt();
+//        System.out.println("angka  :");
+//        int w = g.nextInt();
+//        
+//        if( w >= k && w <= q){
+//            System.out.println("yayayaya");
+//        }else{
+//            System.out.println("nononono");
+//        }
+
+
+
+          //soal 2
+//        System.out.println("masukan nilai :");
+//        int s = g.nextInt();
+//        
+//        if(s > 75){
+//            System.out.println("soal kroco jhi");
+//        }else{
+//            System.out.println("sa jappo ko aik");
+//        }
+
+
+
+            //soal 3
+//          System.out.println("masukan nilai:");
+//          int c = g.nextInt();
+//          
+//          if(c % 2 == 0 && c != 0){
+//              System.out.println("iyaji");
+//          }else if(c != 0){
+//              System.out.println("tidakji");
+//          }
+    }
+}

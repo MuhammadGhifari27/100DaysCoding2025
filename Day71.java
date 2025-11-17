d492cfc2e8e6178d20484e09ce37842c55dd4d6a
Jihyo <muhammadgio938@gmail.com>
2025-11-17 22:30:19 +0800

    Day71.java

---
+++ Day71.java
@@ -0,0 +1,18 @@
+package day1;
+import java.util.Scanner;
+
+public class Day71 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N: ");
+        int N = input.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println(); 
+        }
+    }
+}

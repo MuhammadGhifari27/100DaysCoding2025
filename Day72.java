71e62ed0618816129e5c99cbf1d4537306a59b15
Jihyo <muhammadgio938@gmail.com>
2025-11-18 18:36:05 +0800

    Day72.java
    This program prompts the user for a value N and prints a descending pattern of asterisks, starting from N asterisks down to 1.

---
+++ Day72.java
@@ -0,0 +1,18 @@
+package day1;
+import java.util.Scanner;
+
+public class Day72 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N: ");
+        int N = input.nextInt();
+
+        for (int i = N; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

9a9f959892cb0161999e5b997fb7597d93a567b5
Jihyo <muhammadgio938@gmail.com>
2025-11-27 20:20:04 +0800

    Day81.java

---
+++ Day81.java
@@ -0,0 +1,12 @@
+package day1;
+public class Day81 {
+    public static void main(String[] args) {
+        int angka[] = {5, 10, 15, 20, 25};
+
+        System.out.println("Elemen indeks 0: " + angka[0]);
+        System.out.println("Elemen indeks 1: " + angka[1]);
+        System.out.println("Elemen indeks 2: " + angka[2]);
+        System.out.println("Elemen indeks 3: " + angka[3]);
+        System.out.println("Elemen indeks 4: " + angka[4]);
+    }
+}

043cc8517ffde4ff4bbe1321457aa47e2025ed46
Jihyo <muhammadgio938@gmail.com>
2025-12-04 22:54:55 +0800

    apaini.java

---
+++ apaini.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tetapsemangat;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ss {
+    public static void main(String[] args) {
+        Scanner g = new Scanner (System.in);
+        System.out.println("nama:");
+        String a = g.nextLine();
+        System.out.println("nama:");
+        String b = g.nextLine();
+        System.out.println("nama:");
+        String c = g.nextLine();
+        
+        System.out.println(a+":\\"+b+"\\"+c);
+    }
+}

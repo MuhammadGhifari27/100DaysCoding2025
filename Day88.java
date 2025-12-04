ec7407df75a2a957f65bfbe6b440637ec998380b
Jihyo <muhammadgio938@gmail.com>
2025-12-04 22:47:57 +0800

    Day88.java

---
+++ Day88.java
@@ -0,0 +1,10 @@
+package day1;
+public class Day88 {
+    public static void main(String[] args) {
+        int[] angka = {10, 20, 30, 40, 50};
+
+        for (int n : angka) {
+            System.out.println(n);
+        }
+    }
+}

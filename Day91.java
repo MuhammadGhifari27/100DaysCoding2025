8d426ec5727b65b9b9d75eaf77e7e6a5f47f8598
Jihyo <muhammadgio938@gmail.com>
2025-12-07 10:38:45 +0800

    Day91.java

---
+++ Day91.java
@@ -0,0 +1,10 @@
+package day1;
+public class Day91 {
+    static void sapa(String nama) {
+        System.out.println("Halo " + nama);
+    }
+    public static void main(String[] args) {
+        sapa("Lusyver"); 
+        sapa("Ghifari"); 
+    }
+}

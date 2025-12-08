4ecdf2ec22171e8483d677e7f433d3baba6dd335
Jihyo <muhammadgio938@gmail.com>
2025-12-09 01:03:25 +0800

    Day93.java

---
+++ Day93.java
@@ -0,0 +1,21 @@
+
+package day1;
+        public class Day93 {
+    public static void main(String[] args) {
+
+        double nilai1 = 4.2;
+        double nilai2 = 4.8;
+
+        System.out.println("=== Contoh Math.ceil() ===");
+        System.out.println("ceil(4.2) = " + Math.ceil(nilai1));
+        System.out.println("ceil(4.8) = " + Math.ceil(nilai2));
+
+        System.out.println("\n=== Contoh Math.floor() ===");
+        System.out.println("floor(4.2) = " + Math.floor(nilai1));
+        System.out.println("floor(4.8) = " + Math.floor(nilai2));
+
+        System.out.println("\n=== Contoh Math.round() ===");
+        System.out.println("round(4.2) = " + Math.round(nilai1));
+        System.out.println("round(4.8) = " + Math.round(nilai2));
+    }
+}

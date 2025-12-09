83ffb650a9f5e1c3081c2cd4c8ebe5f8590bc85c
Jihyo <muhammadgio938@gmail.com>
2025-12-10 03:16:36 +0800

    Day94.java

---
+++ Day94.java
@@ -0,0 +1,10 @@
+package day1;
+public class Day94 {
+
+    static void cetakPesan() {
+        System.out.println("Halo, ini adalah pesan dari method void!");
+    }
+    public static void main(String[] args) {
+       cetakPesan();
+    }
+}

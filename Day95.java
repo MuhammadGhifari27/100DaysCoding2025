4331018d9eaf3b1f17104ad7159c0c8b1a6d14f6
Jihyo <muhammadgio938@gmail.com>
2025-12-11 01:07:58 +0800

    Day95.java

---
+++ Day95.java
@@ -0,0 +1,11 @@
+package day1;
+public class Day95 {
+    static void cetakPesan(String pesan) {
+        System.out.println(pesan);
+    }
+    public static void main(String[] args) {
+        cetakPesan("Halo, ini pesan pertama!");
+        cetakPesan("Belajar method dengan parameter itu mudah.");
+        cetakPesan("Semangat Lusyver");
+    }
+}

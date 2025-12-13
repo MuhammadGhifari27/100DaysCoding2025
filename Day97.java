1bb0a5d8f4410884164ca16d7c1f58cf2af7200f
Jihyo <muhammadgio938@gmail.com>
2025-12-13 13:36:49 +0800

    Day97.java

---
+++ Day97.java
@@ -0,0 +1,10 @@
+package day1;
+public class Day97 {
+    static int luasPersegi(int sisi) {
+        return sisi * sisi;
+    }
+    public static void main(String[] args) {
+        int hasil = luasPersegi(6);
+        System.out.println("Luas persegi adalah: " + hasil);
+    }
+}
